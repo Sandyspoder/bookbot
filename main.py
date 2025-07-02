7440a180011c7f26425b31b2e3e03e70656048a2
Sandyspoder <danielnjgatward@outlook.com>
2025-07-02 12:57:09 +0100

    added the character read out and further functionality for checking other authored works

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-from stats import num_words_func
-from stats import num_char_func
+from stats import num_words_func, num_char_func, sort_char_dict
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as file:
         return file.read()
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if not len(sys.argv) == 2:
+        print("Usage: python3 main.py <path_to_book>") 
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+
     book_text = get_book_text(book_path)
     num_char = num_char_func(book_text)
+    sorted_num_char = sort_char_dict(num_char)
     num_words = num_words_func(book_text)
 
 
-    return print(f"{num_words} words found in the document"),print(num_char)
-
+    print(f"============ BOOKBOT ============")
+    print(f"Analyzing book found at books/frankenstein.txt...")
+    print(f"----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print(f"--------- Character Count -------")
+    for i in range(0,len(sorted_num_char)):
+        print(f"{sorted_num_char[i]["char"]}: {sorted_num_char[i]["num"]}")
+    print(f"============= END ===============")
 main()
      
 
--- stats.py
+++ stats.py
@@ -11,5 +11,19 @@ def num_char_func(text):
         elif char not in dict:
             dict[char] = 1
 
+    return dict
+
+def sort_char_dict(dict):
+
+    def sort_on(items):
+        return items["num"]
+
+    char_list = []
+    for i in dict:
+        if i.isalpha():
+            char_list.append({"char":i, "num":dict[i]})
         
-    return dict 
+
+    char_list.sort(reverse=True, key=sort_on)
+        
+    return char_list 
